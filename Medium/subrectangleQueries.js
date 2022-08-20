70a1f4046e3dcb46c504b80465936d7c0f9cb8d1
ToshaEX <tharinduoshan@gmail.com>
2022-08-20 15:52:16 +0530

    first commit

---
+++ Medium/subrectangleQueries.js
@@ -0,0 +1,50 @@
+//1476. Subrectangle Queries
+/**
+ * @param {number[][]} rectangle
+ */
+var SubrectangleQueries = function (rectangle) {
+  this.rectangle = rectangle;
+};
+
+/**
+ * @param {number} row1
+ * @param {number} col1
+ * @param {number} row2
+ * @param {number} col2
+ * @param {number} newValue
+ * @return {void}
+ */
+SubrectangleQueries.prototype.updateSubrectangle = function (
+  row1,
+  col1,
+  row2,
+  col2,
+  newValue
+) {
+  let row = row1;
+  let col = col1;
+  for (; row <= row2; ) {
+    this.rectangle[row][col] = newValue;
+    col++;
+    if (col === col2 + 1) {
+      col = col1;
+      row++;
+    }
+  }
+};
+
+/**
+ * @param {number} row
+ * @param {number} col
+ * @return {number}
+ */
+SubrectangleQueries.prototype.getValue = function (row, col) {
+  return this.rectangle[row][col];
+};
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * var obj = new SubrectangleQueries(rectangle)
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+ * var param_2 = obj.getValue(row,col)
+ */
